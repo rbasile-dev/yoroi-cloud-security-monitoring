b4e850bdabbf821c36eb515e268959d1b0ce3f5e
Richard Basile <rpbasile68@gmail.com>
2026-05-27 16:35:37 -0400

    Save security event to JSON file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
 print("Yoroi initialized")
+
+from datetime import datetime
+import json
+import os
+
+event = {
+    "event_type": "failed_login",
+    "username": "admin",
+    "source_ip": "192.168.1.25",
+    "severity": "medium",
+    "timestamp": datetime.utcnow().isoformat()
+}
+
+os.makedirs("logs", exist_ok=True)
+
+with open("logs/security_event.json", "w") as file:
+    json.dump(event, file, indent=2)
+
+print("Security event saved to logs/security_event.json")
